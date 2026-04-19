a7a48de97787b48cdfe25bc20deacddf1f5cb456
Atzer 722 <kiliancantel@gmail.com>
2026-04-19 17:00:40 +0200

    Codebase: reorganize the codebase using the file separate logic.

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 
 START /MIN CMD.EXE /C powershell -ExecutionPolicy Bypass -File ".\compile-game.ps1"
-START /MIN CMD.EXE /C game-launcher.bat # For launching the game without a cmd window.
+game-launcher.bat # For launching the game without a cmd window.
 
 pause
